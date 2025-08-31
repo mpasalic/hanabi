56767f2eff3e8a7db3c57982f6c6d442ce38692c
Mirza Pasalic <mirza.pasalic@gmail.com>
2025-08-31 01:19:12 -0700

    temp hardcoded url

--- web-client/src/main.rs
+++ web-client/src/main.rs
@@ -115,21 +115,25 @@ impl Default for HelloApp {
     }
 }
 
-#[cfg(target_arch = "wasm32")]
-fn get_websocket_url(cc: &eframe::CreationContext<'_>) -> String {
-    let proto = &cc.integration_info.web_info.location.protocol;
-    let host = &cc.integration_info.web_info.location.host;
-
-    console_log!("Protocol: '{:?}'", proto);
-    console_log!("Host: '{:?}'", host);
-
-    let url = match proto.as_str() {
-        "https:" => format!("wss://{}/websocket", host),
-        _ => format!("ws://{}/websocket", host),
-    };
-
-    url
+fn get_websocket_url(_cc: &eframe::CreationContext<'_>) -> String {
+    "wss://hanabi-ufgm.shuttle.app/websocket".to_string()
 }
+
+// #[cfg(target_arch = "wasm32")]
+// fn get_websocket_url(cc: &eframe::CreationContext<'_>) -> String {
+//     let proto = &cc.integration_info.web_info.location.protocol;
+//     let host = &cc.integration_info.web_info.location.host;
+
+//     console_log!("Protocol: '{:?}'", proto);
+//     console_log!("Host: '{:?}'", host);
+
+//     let url = match proto.as_str() {
+//         "https:" => format!("wss://{}/websocket", host),
+//         _ => format!("ws://{}/websocket", host),
+//     };
+
+//     url
+// }
 
 #[cfg(target_arch = "wasm32")]
 fn get_web_url(cc: &eframe::CreationContext<'_>) -> String {
@@ -153,10 +157,10 @@ fn get_session_id(cc: &eframe::CreationContext<'_>) -> Option<String> {
 static PLAYER_NAME: &str = "player_name";
 
 // When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
-fn get_websocket_url(_cc: &eframe::CreationContext<'_>) -> String {
-    "ws://127.0.0.1:8000/websocket".to_string()
-}
+// #[cfg(not(target_arch = "wasm32"))]
+// fn get_websocket_url(_cc: &eframe::CreationContext<'_>) -> String {
+//     "ws://127.0.0.1:8000/websocket".to_string()
+// }
 
 #[cfg(not(target_arch = "wasm32"))]
 fn get_web_url(_cc: &eframe::CreationContext<'_>) -> String {
